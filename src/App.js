56debfb31b2b7e2f79a9e6036a74f0fec9a3bf82
marianomat <marianopereyra95@gmail.com>
2023-08-28 13:42:02 -0300

    checklogin

--- src/App.js
+++ src/App.js
@@ -7,26 +7,31 @@ import Register from "./pages/register/register";
 import Perfil from "./pages/perfil/perfil";
 import Alquileres from "./pages/alquileres/alquileres";
 import Navbar from "./components/navbar/navbar";
+import CheckAuth from "./components/CheckAuth";
 
 function AppWrapper() {
-	const [isLogged, setIsLogged] = useState(false);
+	const [isLogged, setIsLogged] = useState(null);
 
 	useEffect(() => {
 		if (localStorage.getItem("token")) {
 			setIsLogged(true);
+		} else {
+			setIsLogged(false);
 		}
-	}, []);
+	}, [isLogged]);
 
 	return (
 		<>
 			<Navbar isLogged={isLogged} setIsLogged={setIsLogged} />
 			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/home" element={<Home />} />
+				<Route element={<CheckAuth isLogged={isLogged} />}>
+					<Route index element={<Home />} />
+					<Route path="/home" element={<Home />} />
+					<Route path="/alquileres" element={<Alquileres />} />
+					<Route path="/perfil" element={<Perfil />} />
+				</Route>
 				<Route path="/login" element={<Login setIsLogged={setIsLogged} />} />
 				<Route path="/register" element={<Register setIsLogged={setIsLogged} />} />
-				<Route path="/alquileres" element={<Alquileres />} />
-				<Route path="/perfil" element={<Perfil />} />
 				<Route path="/404" element={<NotFound />} />
 				<Route path="*" element={<NotFound />} />
 			</Routes>
